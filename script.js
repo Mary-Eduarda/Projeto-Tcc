f4cb95085753131d6535a3eb84ddf82da4b56e8b
Duds <meduardarodrigues91@gmail.com>
2025-06-10 09:53:26 -0300

    Criando avisos

--- script.js
+++ script.js
@@ -1,15 +1,29 @@
 //(automaticamente para email do adm)
 
 function enviarTcc() {
-    const inputEnviarTcc = document.getElementById("enviarTcc");
-    let tcc = inputEnviarTcc.value.trim(); // Obtendo o valor do input e removendo espaços em branco
-    const mensagemElemento = document.getElementById("mensgagem");
-  
-    // Verificando se o usuário adicionou algo
-    if (tcc === "") {
-      alert("Digite em todos os campos, para que seu TCC seja enviado!");
-      return false;}
-       else {
-      alert("Seu TCC foi enviado com sucesso!");
-    }
+
+
+    //CRIANDO AVISO DE DADOS INCOMPLETOS parte 1 (criando alerta)
+    
+    const enviarTcc = document.getElementById('enviarTcc'); //pega o documento inteiro, pra que, analisando o botão de enviar para que todos os campos tenham sido preenchidos
+ 
+    //Adicionando um "ouvinte de evento" ao formulário, quando o botão for clicado
+    formTCC.addEventListener = 'submit', function(event) {
+  event.preventDefault(); //previne de recarregar a página ao enviar o formulário
+  }  
+
+   //CRIANDO AVISO DE DADOS INCOMPLETOS parte 2 (definindo locais)
+    const nometextarea = document.getElementById('nometextarea'); //analisando o nome
+    const titulotextarea = document.getElementById('titulotextarea'); //analisando o título
+    const textotcc = document.getElementById('textotcc'); //analisando o texto do TCC
+
+     //CRIANDO AVISO DE DADOS INCOMPLETOS parte 1 (verificando se os locais estão vazios)
+
+       if (nometextarea.value === '' || titulotextarea.value === '' || textotcc.value === '') {
+        // Se estuver incompleto, exibe este alerta do navegador
+        alert('Campos incompletos! Para prosseguir, é necessário preencher todas as informações solicitadas.');
+    } else {
+        event.target.submit(); 
+
   }
+}
